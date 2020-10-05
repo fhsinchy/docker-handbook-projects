6d4a672d7063e4c22b8dc53cb2977e26ae55b388
Farhan Hasin Chowdhury <mail@farhan.info>
2020-10-05 15:55:20 +0600

    Updated boot.sh and using migrations on prod

--- fullstack-notes-application/boot.sh
+++ fullstack-notes-application/boot.sh
@@ -1,69 +1,68 @@
 #!/bin/bash
+set -e
 
 printf "creating network --->\n"
-docker network create fullstack-note-application-network
+docker network create fullstack-notes-application-network
 printf "network created --->\n"
 
 printf "\n"
 
-cd db; \
-printf "creating db image --->\n"
-docker image build . --tag notes-db; \
-printf "db image created --->\n"
+cd db;
 printf "starting db container --->\n"
 docker container run \
     --detach \
     --name=db \
     --env POSTGRES_DB=notesdb \
     --env POSTGRES_PASSWORD=secret \
-    --network=fullstack-note-application-network \
-    notes-db;
+    --network=fullstack-notes-application-network \
+    postgres:12;
 printf "db container started --->\n"
 
 cd ..
 printf "\n"
 
-cd api; \
+cd api;
 printf "creating api image --->\n"
-docker image build . --tag notes-api; \
+docker image build . --tag notes-api
 printf "api image created --->\n"
 printf "starting api container --->\n"
 docker container run \
     --detach \
     --name=api \
     --env-file .env \
-    --network=fullstack-note-application-network \
+    --network=fullstack-notes-application-network \
     notes-api;
+docker container exec api npm run db:migrate;
 printf "api container started --->\n"
 
 cd ..
 printf "\n"
 
-cd client; \
+cd client;
 printf "creating client image --->\n"
-docker image build . --tag notes-client; \
+docker image build . --tag notes-client
 printf "client image created --->\n"
 printf "starting client container --->\n"
 docker container run \
     --detach \
     --name=client \
-    --network=fullstack-note-application-network \
+    --network=fullstack-notes-application-network \
     notes-client;
 printf "client container started --->\n"
 
 cd ..
 printf "\n"
 
-cd nginx; \
+cd nginx;
 printf "creating router image --->\n"
-docker image build . --tag notes-router; \
+docker image build . --tag notes-router
 printf "router image created --->\n"
 printf "starting router container --->\n"
 docker container run \
     --detach \
     --name=router \
     --publish=8080:80 \
-    --network=fullstack-note-application-network \
+    --network=fullstack-notes-application-network \
     notes-router;
 printf "router container started --->\n"
 
--- fullstack-notes-application/db/docker-entrypoint-initdb.d/notes.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS notes (
-    id serial PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    content text NOT NULL
-);
