fd48644ca67ac4b669c6e0ed5ecae9c8f52f32bb
Farhan Hasin Chowdhury <mail@farhan.info>
2021-01-09 01:13:41 +0600

    Updated container names

--- notes-api/boot.sh
+++ notes-api/boot.sh
@@ -10,7 +10,7 @@ printf "\n"
 printf "starting db container --->\n"
 docker container run \
     --detach \
-    --name=db \
+    --name=notes-db \
     --env POSTGRES_DB=notesdb \
     --env POSTGRES_PASSWORD=secret \
     --network=notes-api-network \
@@ -26,12 +26,12 @@ printf "api image created --->\n"
 printf "starting api container --->\n"
 docker container run \
     --detach \
-    --name=api \
+    --name=notes-api \
     --env-file .env \
     --publish=3000:3000 \
     --network=notes-api-network \
     notes-api;
-docker container exec api npm run db:migrate;
+docker container exec notes-api npm run db:migrate;
 printf "api container started --->\n"
 
 cd ..
